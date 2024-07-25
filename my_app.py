ce19b251219aaa71325bb7f790bc6a69f1312c83
ristero <sadovyjd9@gmail.com>
2024-07-25 19:59:08 +0200

    0.2

--- my_app.py
+++ my_app.py
@@ -2,8 +2,7 @@ from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget,QPushButton, QVBoxLayout,QLabel 
 from instr import*
 from second_win import*
-'''print(txt_title)
-print(hello)'''
+
 
 class MainWin(QWidget):
     def __init__(self):
