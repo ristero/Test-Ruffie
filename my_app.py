4d4d4996e1afb7c75bb703ed039226ebcfccfac6
ristero <sadovyjd9@gmail.com>
2024-07-25 19:57:00 +0200

    0.1

--- my_app.py
+++ my_app.py
@@ -1,1 +1,42 @@
-# напиши тут код основної програми та першого екрану
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget,QPushButton, QVBoxLayout,QLabel 
+from instr import*
+from second_win import*
+'''print(txt_title)
+print(hello)'''
+
+class MainWin(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.set_appear()
+        self.initUI()
+        self.connects()
+        self.show()
+    def set_appear(self):
+        self.setWindowTitle(txt_title)
+        self.resize(h,b)
+    def initUI(self):
+        self.text1 = QLabel(hello)
+        self.text2 = QLabel(instraction)
+        self.pbtn = QPushButton(pbtn_text)
+        self.v_line = QVBoxLayout()
+        self.widgets = [self.text1,self.text2,self.pbtn]
+        self.add_widgets()
+        self.setLayout(self.v_line)
+    def add_widgets(self):
+        for i in self.widgets:
+            self.v_line.addWidget(i,alignment= Qt.AlignCenter)
+
+
+    def connects(self):
+        self.pbtn.clicked.connect(self.next_click)
+    def next_click(self):
+        self.hide()
+        self.nw = SecondWin()
+
+
+
+app = QApplication([])
+win = MainWin()
+app.exec_()
+
